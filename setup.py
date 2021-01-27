ca2487992ed5f897c270dd8e9ae8f8392387387d
Pierre Rouanet <pierre.rouanet@gmail.com>
2021-01-27 17:35:10 +0100

    Add Otis as a part.

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, '..', 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='reachy',
+    version='1.0.0',
+    packages=find_packages(exclude=['tests']),
+
+    install_requires=[
+        'reachy',
+        'numpy',
+    ],
+
+    author='Pollen Robotics',
+    author_email='contact@pollen-robotics.com',
+    url='https://github.com/pollen-robotics/otis',
+
+    description='Otis: handwriting end-effector for Reachy.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
