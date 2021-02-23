640d69bff76aaa900910ab1cbb8c3bda08c20e16
Pierre Rouanet <pierre.rouanet@gmail.com>
2021-02-23 18:02:31 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='reachy-otis',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(exclude=['tests']),
 
     install_requires=[
